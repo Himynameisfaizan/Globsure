d8f0a0be42d24f9bca4e381f0d3aa385e34dff96
Himynameisfaizan <faizansaikh830@gmail.com>
2025-12-12 11:05:04 +0530

    making responsive contact page

--- src/components/contact-page/Landing_page.jsx
+++ src/components/contact-page/Landing_page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './styling/responsive.css'
 const Landing_page = ({bg_image, landing_title}) => {
   return (
     <>
@@ -13,7 +14,7 @@ const Landing_page = ({bg_image, landing_title}) => {
             <div className="front w-full h-full bg-linear-to-r from-[#00224991] from-48% to-transparent to-60%"></div>
             <div className="text absolute top-30 left-20 flex flex-col items-center">
             <h1 style={{padding:'10px 0'}} className='text-5xl text-white'>{landing_title}</h1>
-            <h2 className='text-lxg text-white'> <Link className='hover:text-[#002249bd] duration-250' to='/'> Home </Link><span style={{padding:"0px 10px"}}>/</span> {landing_title}</h2>
+            <h2 className='text-lg text-white'> <Link className='hover:text-[#002249bd] duration-250' to='/'> Home </Link><span style={{padding:"0px 10px"}}>/</span> {landing_title}</h2>
             </div>
         </div>
     </>
