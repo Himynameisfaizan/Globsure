f4db84520ab58386d49fb0b9790751c371032d4f
Himynameisfaizan <faizansaikh830@gmail.com>
2025-12-13 15:16:18 +0530

    making responsive insurance page

--- src/components/insurnce-page/health/Accordion.jsx
+++ src/components/insurnce-page/health/Accordion.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import './responsive.css'
 const Accordion = ({ faq }) => {
   const [activeTab, setActiveTab] = useState(null); 
 
@@ -15,14 +15,14 @@ const Accordion = ({ faq }) => {
         return (
           <div
             key={data.id}
-            className="bg-[#0073bd20] border border-[#00224925] w-full rounded-lg flex flex-col gap-4" 
+            className="accordion-section bg-[#0073bd20] border border-[#00224925] w-full rounded-lg flex flex-col gap-4" 
             style={{ padding: "15px" }}  >
 
             <div 
               className="flex justify-between cursor-pointer items-center mb-2" 
               onClick={() => toggleTab(data.id)}
             >
-              <h2 className="text-[#002249] text-lg font-medium">{data.id}.   {data.question}</h2>
+              <h2 className="h2 text-[#002249] text-lg font-medium">{data.id}.   {data.question}</h2>
               
               <i 
                 className={`text-xl ri-arrow-down-s-fill transition-transform duration-300 ${isActive ? "rotate-180" : "rotate-0"}`}
@@ -35,7 +35,7 @@ const Accordion = ({ faq }) => {
               }`}
             >
               <div className="overflow-hidden flex gap-2">
-              <i className="text-[#0073bd] ri-arrow-right-long-line"></i>  <p className="text-[#000000a6] text-base pt-2">
+              <i className="text-[#0073bd] ri-arrow-right-long-line"></i>  <p className="para text-[#000000a6] text-base pt-2">
                   {data.answer}
                 </p>
               </div>
